e2e3d48efff5d69c7950c285a19809a98789b66e
Lissa2121 <114819609+Lissa2121@users.noreply.github.com>
2025-01-31 23:57:30 +0200

    hw1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Header = () => {
+  return <header>
+      <div>logo</div>
+      <nav>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </nav>
+    </header>
 }
 
-export default App
+const Section = () => {
+  return <section>
+      <h2>Hi Me!</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum officiis hic optio? Expedita, eveniet totam a facere magni dolorum. Nihil nostrum earum aspernatur explicabo odit aliquam repellat facilis. Eveniet, soluta.
+        Velit fugit assumenda aliquam, provident et inventore corrupti exercitationem mollitia quas distinctio, aliquid sit, consequatur ex earum cupiditate est harum sapiente voluptatum eligendi. Dolorum quibusdam necessitatibus molestias cumque sunt dolores?</p>
+      
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+
+    </section>
+}
+
+const Footer = () => {
+  return <footer>
+      <span> Finall!</span>
+    </footer>
+}
+
+const App = () => {
+  return <div>
+    <Header />
+    <Section />
+    <Footer />
+
+    </div>;
+    
+
+};
+
+export default App;
